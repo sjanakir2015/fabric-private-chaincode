9fa2a4e92af5180e51dbab8d8ed8eaee7432a9d7
Swetha Repakula <srepaku@us.ibm.com>
2019-11-22 13:25:59 -0800

    wip

--- demo/scripts/installCC.sh
+++ demo/scripts/installCC.sh
@@ -1,10 +1,9 @@
+#!/bin/bash
 #
 # Copyright IBM Corp All Rights Reserved
 #
 # SPDX-License-Identifier: Apache-2.0
-#
 
-#!/bin/bash
 
 #  There are 2 installs, followed by (instantiate and upgrade)
 #  with (install + instantiate) executed once, the _correct_ chaincode was not getting installed
@@ -15,12 +14,18 @@
 #  This version works.  TODO:  To come back and identify the reason for this error
 #  above comment is not valid anymore;  But this version works !  
 set -ev
+
 export WAIT_TIME=15
 export version=1.1
+export FABRIC_BIN_DIR=/project/src/github.com/hyperledger/fabric/.build/bin
+export PEER_CMD=/project/src/github.com/hyperledger-labs/fabric-private-chaincode/fabric/bin/peer.sh
+export FPC_DIR=/project/src/github.com/hyperledger-labs/fabric-private-chaincode
 
-docker image prune -f
-docker exec cli peer chaincode install -n mockcc -v ${version} --path /opt/chaincode/javascript  -l NODE
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com ${PEER_CMD} chaincode install -n mockcc -v nov-1129 --path ${FPC_DIR}/demo/chaincode/javascript  -l NODE
 sleep ${WAIT_TIME} 
 
-docker exec cli peer chaincode instantiate -n mockcc -v ${version}  --channelID mychannel -o orderer.example.com:7050 -c '{"Args":[]}' 
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" -e "ORDERER_ADDR=orderer.example.com:7050" peer0.org1.example.com ${PEER_CMD} chaincode instantiate -n mockcc -v nov-1129 --channelID mychannel -o orderer.example.com:7050 -c '{"Args":[]}'
 sleep ${WAIT_TIME} 
+
+
+
--- demo/scripts/startAuctionNetwork.sh
+++ demo/scripts/startAuctionNetwork.sh
@@ -1,39 +1,38 @@
+#!/bin/bash
 #
 # Copyright IBM Corp All Rights Reserved
 #
 # SPDX-License-Identifier: Apache-2.0
-#
 
-#!/bin/bash
+export DEMO_SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export FPC_ROOT=${DEMO_SCRIPTS_DIR}/../..
+export NETWORK_DIR=${FPC_ROOT}/utils/docker-compose/
+export CONTAINER_FPC=/project/src/github.com/hyperledger-labs/fabric-private-chaincode
 
 #  Possible improvements
 #    During cleanup prior to start of the scripts, some error messages appear. 
 #    Hide error conditions 
 
-#  clean up containers from previous run
-./clearContainers.sh  
-
 set -ev
 # generate crypto material, genesis blocks
 # start Fabric network (1 Org), create and join channel, mychannel
-cd ../network
-./startFabric.sh 
+${NETWORK_DIR}/scripts/generate.sh
+${NETWORK_DIR}/scripts/start.sh
 
 #install and instantiate chaincode
-cd ../scripts 
-./installCC.sh 
+${DEMO_SCRIPTS_DIR}/installCC.sh 
 
 # start client container
-cd ../client
-make run 
-sleep 5 
+# cd ../client
+# make run 
+# sleep 5 
 
-# start frontend container
-cd ../frontend
-make run 
-sleep 5
+# # start frontend container
+# cd ../frontend
+# make run 
+# sleep 5
 
-# test: Submit a few transactions to client
-cd ../scripts 
-./submitTx.sh 
+# # test: Submit a few transactions to client
+# cd ../scripts 
+# ./submitTx.sh 
 
