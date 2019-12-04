d6b2df7620427d4d21ce0fc2a2b24f99e7b6e8cc
Sowmya Janakiraman <sowmya@ibm.com>
2019-12-03 20:55:06 -0600

    Http header changed to "x-user"

--- clock_auction/client/backend/src/ccRoute.js
+++ clock_auction/client/backend/src/ccRoute.js
@@ -27,7 +27,7 @@ ChaincodeRouter.route('/invoke').post(function (request, response) {
 
   //  get userName from "Authorization" header
   //  decode from base64
-  var userName = ( request.headers['authorization']);
+  var userName = ( request.headers['x-user']);
 
   //  decode it
   //userName  = Buffer.from(userName, 'base64').toString('utf-8');
@@ -66,7 +66,7 @@ ChaincodeRouter.route('/query').post(function (request, response) {
 
   //  get userName from "Authorization" header
   //  decode from base64
-  var userName = ( request.headers['authorization']);
+  var userName = ( request.headers['x-user']);
 
   //  decode it
   //userName  = Buffer.from(userName, 'base64').toString('utf-8');
--- clock_auction/scripts/submitTx.sh
+++ clock_auction/scripts/submitTx.sh
@@ -12,78 +12,78 @@ set -ev
 curl -H "Content-Type: application/json" -X GET  http://localhost:3000/api/getRegisteredUsers
 
 # userName = A-Telecom;
-curl -H "Content-Type: application/json" -H "authorization:A-Telecom" -X POST -d '{"tx":"createAuction","args":["unused"]}' http://localhost:3000/api/cc/invoke
+curl -H "Content-Type: application/json" -H "x-user:A-Telecom" -X POST -d '{"tx":"createAuction","args":["unused"]}' http://localhost:3000/api/cc/invoke
 
 
-curl -H "Content-Type: application/json" -H "authorization:A-Telecom" -X POST -d '{"tx":"getAuctionDetails","args":["unused"]}' http://localhost:3000/api/cc/invoke
+curl -H "Content-Type: application/json" -H "x-user:A-Telecom" -X POST -d '{"tx":"getAuctionDetails","args":["unused"]}' http://localhost:3000/api/cc/invoke
 
 
-curl -H "Content-Type: application/json" -H "authorization:A-Telecom" -X POST -d '{"tx":"getAuctionStatus","args":["unused"]}' http://localhost:3000/api/cc/invoke
+curl -H "Content-Type: application/json" -H "x-user:A-Telecom" -X POST -d '{"tx":"getAuctionStatus","args":["unused"]}' http://localhost:3000/api/cc/invoke
 
 
-curl -H "Content-Type: application/json" -H "authorization:A-Telecom" -X POST -d '{"tx":"startNextRound","args":["unused"]}' http://localhost:3000/api/cc/invoke
+curl -H "Content-Type: application/json" -H "x-user:A-Telecom" -X POST -d '{"tx":"startNextRound","args":["unused"]}' http://localhost:3000/api/cc/invoke
 
 
-curl -H "Content-Type: application/json" -H "authorization:A-Telecom" -X POST -d '{"tx":"endRound","args":["unused"]}' http://localhost:3000/api/cc/invoke
+curl -H "Content-Type: application/json" -H "x-user:A-Telecom" -X POST -d '{"tx":"endRound","args":["unused"]}' http://localhost:3000/api/cc/invoke
 
-curl -H "Content-Type: application/json" -H "authorization:A-Telecom" -X POST -d '{"tx":"submitInitialClockBid","args":["unused"]}' http://localhost:3000/api/cc/invoke
+curl -H "Content-Type: application/json" -H "x-user:A-Telecom" -X POST -d '{"tx":"submitInitialClockBid","args":["unused"]}' http://localhost:3000/api/cc/invoke
 
 #  userName = B-Net;
-curl -H "Content-Type: application/json" -H "authorization:B-Net" -X POST -d '{"tx":"submitRegularClockBid","args":["unused"]}' http://localhost:3000/api/cc/invoke
+curl -H "Content-Type: application/json" -H "x-user:B-Net" -X POST -d '{"tx":"submitRegularClockBid","args":["unused"]}' http://localhost:3000/api/cc/invoke
 
 
-curl -H "Content-Type: application/json" -H "authorization:B-Net" -X POST -d '{"tx":"getRoundInfo","args":["unused"]}' http://localhost:3000/api/cc/invoke
+curl -H "Content-Type: application/json" -H "x-user:B-Net" -X POST -d '{"tx":"getRoundInfo","args":["unused"]}' http://localhost:3000/api/cc/invoke
 
 
-curl -H "Content-Type: application/json" -H "authorization:B-Net" -X POST -d '{"tx":"getBidderRoundResults","args":["unused"]}' http://localhost:3000/api/cc/invoke
+curl -H "Content-Type: application/json" -H "x-user:B-Net" -X POST -d '{"tx":"getBidderRoundResults","args":["unused"]}' http://localhost:3000/api/cc/invoke
 
 
-curl -H "Content-Type: application/json" -H "authorization:B-Net" -X POST -d '{"tx":"getOwnerRoundResults","args":["unused"]}' http://localhost:3000/api/cc/invoke
+curl -H "Content-Type: application/json" -H "x-user:B-Net" -X POST -d '{"tx":"getOwnerRoundResults","args":["unused"]}' http://localhost:3000/api/cc/invoke
 
 
-curl -H "Content-Type: application/json" -H "authorization:B-Net" -X POST -d '{"tx":"submitAssignBid","args":["unused"]}' http://localhost:3000/api/cc/invoke
+curl -H "Content-Type: application/json" -H "x-user:B-Net" -X POST -d '{"tx":"submitAssignBid","args":["unused"]}' http://localhost:3000/api/cc/invoke
 
 
 
-curl -H "Content-Type: application/json" -H "authorization:B-Net" -X POST -d '{"tx":"getAssignmentResults","args":["unused"]}' http://localhost:3000/api/cc/invoke
+curl -H "Content-Type: application/json" -H "x-user:B-Net" -X POST -d '{"tx":"getAssignmentResults","args":["unused"]}' http://localhost:3000/api/cc/invoke
 
 
 #............  Queries  ..............
 
 # userName = "C-Mobile"
-curl -H "Content-Type: application/json" -H "authorization:C-Mobile" -X POST -d '{"tx":"createAuction","args":["unused"]}' http://localhost:3000/api/cc/query
+curl -H "Content-Type: application/json" -H "x-user:C-Mobile" -X POST -d '{"tx":"createAuction","args":["unused"]}' http://localhost:3000/api/cc/query
 
 
-curl -H "Content-Type: application/json" -H "authorization:C-Mobile" -X POST -d '{"tx":"getAuctionDetails","args":["unused"]}' http://localhost:3000/api/cc/query
+curl -H "Content-Type: application/json" -H "x-user:C-Mobile" -X POST -d '{"tx":"getAuctionDetails","args":["unused"]}' http://localhost:3000/api/cc/query
 
 
-curl -H "Content-Type: application/json" -H "authorization:C-Mobile" -X POST -d '{"tx":"getAuctionStatus","args":["unused"]}' http://localhost:3000/api/cc/query
+curl -H "Content-Type: application/json" -H "x-user:C-Mobile" -X POST -d '{"tx":"getAuctionStatus","args":["unused"]}' http://localhost:3000/api/cc/query
 
 
-curl -H "Content-Type: application/json" -H "authorization:C-Mobile" -X POST -d '{"tx":"startNextRound","args":["unused"]}' http://localhost:3000/api/cc/query
+curl -H "Content-Type: application/json" -H "x-user:C-Mobile" -X POST -d '{"tx":"startNextRound","args":["unused"]}' http://localhost:3000/api/cc/query
 
-curl -H "Content-Type: application/json" -H "authorization:C-Mobile" -X POST -d '{"tx":"endRound","args":["unused"]}' http://localhost:3000/api/cc/query
+curl -H "Content-Type: application/json" -H "x-user:C-Mobile" -X POST -d '{"tx":"endRound","args":["unused"]}' http://localhost:3000/api/cc/query
 
 
-curl -H "Content-Type: application/json" -H "authorization:C-Mobile" -X POST -d '{"tx":"submitInitialClockBid","args":["unused"]}' http://localhost:3000/api/cc/query
+curl -H "Content-Type: application/json" -H "x-user:C-Mobile" -X POST -d '{"tx":"submitInitialClockBid","args":["unused"]}' http://localhost:3000/api/cc/query
 
 
 #  "userName": "Auctioneer1"
-curl -H "Content-Type: application/json" -H "authorization:Auctioneer1" -X POST -d '{"tx":"submitRegularClockBid","args":["unused"]}' http://localhost:3000/api/cc/query
+curl -H "Content-Type: application/json" -H "x-user:Auctioneer1" -X POST -d '{"tx":"submitRegularClockBid","args":["unused"]}' http://localhost:3000/api/cc/query
 
 
-curl -H "Content-Type: application/json" -H "authorization:Auctioneer1" -X POST -d '{"tx":"getRoundInfo","args":["unused"]}' http://localhost:3000/api/cc/query
+curl -H "Content-Type: application/json" -H "x-user:Auctioneer1" -X POST -d '{"tx":"getRoundInfo","args":["unused"]}' http://localhost:3000/api/cc/query
 
 
-curl -H "Content-Type: application/json" -H "authorization:Auctioneer1" -X POST -d '{"tx":"getBidderRoundResults","args":["unused"]}' http://localhost:3000/api/cc/query
-
-
-
-curl -H "Content-Type: application/json" -H "authorization:Auctioneer1" -X POST -d '{"tx":"getOwnerRoundResults","args":["unused"]}' http://localhost:3000/api/cc/query
+curl -H "Content-Type: application/json" -H "x-user:Auctioneer1" -X POST -d '{"tx":"getBidderRoundResults","args":["unused"]}' http://localhost:3000/api/cc/query
 
 
 
-curl -H "Content-Type: application/json" -H "authorization:Auctioneer1" -X POST -d '{"tx":"submitAssignBid","args":["unused"]}' http://localhost:3000/api/cc/query
+curl -H "Content-Type: application/json" -H "x-user:Auctioneer1" -X POST -d '{"tx":"getOwnerRoundResults","args":["unused"]}' http://localhost:3000/api/cc/query
 
 
-curl -H "Content-Type: application/json" -H "authorization:Auctioneer1" -X POST -d '{"tx":"getAssignmentResults","args":["unused"]}' http://localhost:3000/api/cc/query
+
+curl -H "Content-Type: application/json" -H "x-user:Auctioneer1" -X POST -d '{"tx":"submitAssignBid","args":["unused"]}' http://localhost:3000/api/cc/query
+
+
+curl -H "Content-Type: application/json" -H "x-user:Auctioneer1" -X POST -d '{"tx":"getAssignmentResults","args":["unused"]}' http://localhost:3000/api/cc/query
